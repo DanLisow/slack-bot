171e1a75a5fb38e47773130e1c96ac81a7f023df
Maxim Zadumin <recentfear@yandex.ru>
2020-08-29 17:19:31 +0500

    md5 was changed on bcrypt

--- src/main/java/ru/BoshkaLab/services/UserService.java
+++ src/main/java/ru/BoshkaLab/services/UserService.java
@@ -1,33 +1,27 @@
 package ru.BoshkaLab.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.DigestUtils;
 import ru.BoshkaLab.entities.User;
 import ru.BoshkaLab.entities.UserType;
 import ru.BoshkaLab.repositories.UserRepository;
 
 @Service
 public class UserService{
-    private static final String salt = "a6KzG3y1kcRp4";
-
     @Autowired
     private UserRepository userRepository;
 
     public void add(String login, String password, String email,
                     String name, String surname, UserType userType) {
-        User newUser = new User(login, getMD5Hash(password), email,
-                                name, surname, userType);
+        User newUser = new User(login, new BCryptPasswordEncoder().encode(password),
+                                email, name, surname, userType);
         userRepository.saveAndFlush(newUser);
     }
 
     public boolean auth(String loginOrEmail, String password) {
-        String passwordHash = getMD5Hash(password);
+        String passwordHash = new BCryptPasswordEncoder().encode(password);
         return  userRepository.existsByLoginAndPassword(loginOrEmail, passwordHash) ||
                 userRepository.existsByEmailAndPassword(loginOrEmail, passwordHash);
-    }
-
-    private String getMD5Hash(String password) {
-        return DigestUtils.md5DigestAsHex((password + salt).getBytes());
     }
 }
